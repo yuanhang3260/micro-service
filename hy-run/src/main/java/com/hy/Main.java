db5a0a0b852946fac3d7e4c2c10fa9c2120ce8d5
yuanhang3260 <yuanhang3260@gmail.com>
2019-10-24 02:15:17 +0800

    update

---
+++ hy-run/src/main/java/com/hy/Main.java
@@ -0,0 +1,9 @@
+package com.hy;
+
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("hello hy");
+  }
+
+}
---
+++ proto/src/generated/main/grpc/com/hy/user/pb/UserServiceGrpc.java
@@ -0,0 +1,262 @@
+package com.hy.user.pb;
+
+import static io.grpc.stub.ClientCalls.asyncUnaryCall;
+import static io.grpc.stub.ClientCalls.asyncServerStreamingCall;
+import static io.grpc.stub.ClientCalls.asyncClientStreamingCall;
+import static io.grpc.stub.ClientCalls.asyncBidiStreamingCall;
+import static io.grpc.stub.ClientCalls.blockingUnaryCall;
+import static io.grpc.stub.ClientCalls.blockingServerStreamingCall;
+import static io.grpc.stub.ClientCalls.futureUnaryCall;
+import static io.grpc.MethodDescriptor.generateFullMethodName;
+import static io.grpc.stub.ServerCalls.asyncUnaryCall;
+import static io.grpc.stub.ServerCalls.asyncServerStreamingCall;
+import static io.grpc.stub.ServerCalls.asyncClientStreamingCall;
+import static io.grpc.stub.ServerCalls.asyncBidiStreamingCall;
+import static io.grpc.stub.ServerCalls.asyncUnimplementedUnaryCall;
+import static io.grpc.stub.ServerCalls.asyncUnimplementedStreamingCall;
+
+/**
+ */
+@javax.annotation.Generated(
+    value = "by gRPC proto compiler (version 1.7.0)",
+    comments = "Source: user/User.proto")
+public final class UserServiceGrpc {
+
+  private UserServiceGrpc() {}
+
+  public static final String SERVICE_NAME = "hy.user.UserService";
+
+  // Static method descriptors that strictly reflect the proto.
+  @io.grpc.ExperimentalApi("https://github.com/grpc/grpc-java/issues/1901")
+  public static final io.grpc.MethodDescriptor<com.hy.user.pb.UserInfoRequest,
+      com.hy.user.pb.UserInfoResponse> METHOD_GET_USER_BY_NAME =
+      io.grpc.MethodDescriptor.<com.hy.user.pb.UserInfoRequest, com.hy.user.pb.UserInfoResponse>newBuilder()
+          .setType(io.grpc.MethodDescriptor.MethodType.UNARY)
+          .setFullMethodName(generateFullMethodName(
+              "hy.user.UserService", "getUserByName"))
+          .setRequestMarshaller(io.grpc.protobuf.ProtoUtils.marshaller(
+              com.hy.user.pb.UserInfoRequest.getDefaultInstance()))
+          .setResponseMarshaller(io.grpc.protobuf.ProtoUtils.marshaller(
+              com.hy.user.pb.UserInfoResponse.getDefaultInstance()))
+          .setSchemaDescriptor(new UserServiceMethodDescriptorSupplier("getUserByName"))
+          .build();
+
+  /**
+   * Creates a new async stub that supports all call types for the service
+   */
+  public static UserServiceStub newStub(io.grpc.Channel channel) {
+    return new UserServiceStub(channel);
+  }
+
+  /**
+   * Creates a new blocking-style stub that supports unary and streaming output calls on the service
+   */
+  public static UserServiceBlockingStub newBlockingStub(
+      io.grpc.Channel channel) {
+    return new UserServiceBlockingStub(channel);
+  }
+
+  /**
+   * Creates a new ListenableFuture-style stub that supports unary calls on the service
+   */
+  public static UserServiceFutureStub newFutureStub(
+      io.grpc.Channel channel) {
+    return new UserServiceFutureStub(channel);
+  }
+
+  /**
+   */
+  public static abstract class UserServiceImplBase implements io.grpc.BindableService {
+
+    /**
+     */
+    public void getUserByName(com.hy.user.pb.UserInfoRequest request,
+        io.grpc.stub.StreamObserver<com.hy.user.pb.UserInfoResponse> responseObserver) {
+      asyncUnimplementedUnaryCall(METHOD_GET_USER_BY_NAME, responseObserver);
+    }
+
+    @java.lang.Override public final io.grpc.ServerServiceDefinition bindService() {
+      return io.grpc.ServerServiceDefinition.builder(getServiceDescriptor())
+          .addMethod(
+            METHOD_GET_USER_BY_NAME,
+            asyncUnaryCall(
+              new MethodHandlers<
+                com.hy.user.pb.UserInfoRequest,
+                com.hy.user.pb.UserInfoResponse>(
+                  this, METHODID_GET_USER_BY_NAME)))
+          .build();
+    }
+  }
+
+  /**
+   */
+  public static final class UserServiceStub extends io.grpc.stub.AbstractStub<UserServiceStub> {
+    private UserServiceStub(io.grpc.Channel channel) {
+      super(channel);
+    }
+
+    private UserServiceStub(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @java.lang.Override
+    protected UserServiceStub build(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      return new UserServiceStub(channel, callOptions);
+    }
+
+    /**
+     */
+    public void getUserByName(com.hy.user.pb.UserInfoRequest request,
+        io.grpc.stub.StreamObserver<com.hy.user.pb.UserInfoResponse> responseObserver) {
+      asyncUnaryCall(
+          getChannel().newCall(METHOD_GET_USER_BY_NAME, getCallOptions()), request, responseObserver);
+    }
+  }
+
+  /**
+   */
+  public static final class UserServiceBlockingStub extends io.grpc.stub.AbstractStub<UserServiceBlockingStub> {
+    private UserServiceBlockingStub(io.grpc.Channel channel) {
+      super(channel);
+    }
+
+    private UserServiceBlockingStub(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @java.lang.Override
+    protected UserServiceBlockingStub build(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      return new UserServiceBlockingStub(channel, callOptions);
+    }
+
+    /**
+     */
+    public com.hy.user.pb.UserInfoResponse getUserByName(com.hy.user.pb.UserInfoRequest request) {
+      return blockingUnaryCall(
+          getChannel(), METHOD_GET_USER_BY_NAME, getCallOptions(), request);
+    }
+  }
+
+  /**
+   */
+  public static final class UserServiceFutureStub extends io.grpc.stub.AbstractStub<UserServiceFutureStub> {
+    private UserServiceFutureStub(io.grpc.Channel channel) {
+      super(channel);
+    }
+
+    private UserServiceFutureStub(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @java.lang.Override
+    protected UserServiceFutureStub build(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      return new UserServiceFutureStub(channel, callOptions);
+    }
+
+    /**
+     */
+    public com.google.common.util.concurrent.ListenableFuture<com.hy.user.pb.UserInfoResponse> getUserByName(
+        com.hy.user.pb.UserInfoRequest request) {
+      return futureUnaryCall(
+          getChannel().newCall(METHOD_GET_USER_BY_NAME, getCallOptions()), request);
+    }
+  }
+
+  private static final int METHODID_GET_USER_BY_NAME = 0;
+
+  private static final class MethodHandlers<Req, Resp> implements
+      io.grpc.stub.ServerCalls.UnaryMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.ServerStreamingMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.ClientStreamingMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.BidiStreamingMethod<Req, Resp> {
+    private final UserServiceImplBase serviceImpl;
+    private final int methodId;
+
+    MethodHandlers(UserServiceImplBase serviceImpl, int methodId) {
+      this.serviceImpl = serviceImpl;
+      this.methodId = methodId;
+    }
+
+    @java.lang.Override
+    @java.lang.SuppressWarnings("unchecked")
+    public void invoke(Req request, io.grpc.stub.StreamObserver<Resp> responseObserver) {
+      switch (methodId) {
+        case METHODID_GET_USER_BY_NAME:
+          serviceImpl.getUserByName((com.hy.user.pb.UserInfoRequest) request,
+              (io.grpc.stub.StreamObserver<com.hy.user.pb.UserInfoResponse>) responseObserver);
+          break;
+        default:
+          throw new AssertionError();
+      }
+    }
+
+    @java.lang.Override
+    @java.lang.SuppressWarnings("unchecked")
+    public io.grpc.stub.StreamObserver<Req> invoke(
+        io.grpc.stub.StreamObserver<Resp> responseObserver) {
+      switch (methodId) {
+        default:
+          throw new AssertionError();
+      }
+    }
+  }
+
+  private static abstract class UserServiceBaseDescriptorSupplier
+      implements io.grpc.protobuf.ProtoFileDescriptorSupplier, io.grpc.protobuf.ProtoServiceDescriptorSupplier {
+    UserServiceBaseDescriptorSupplier() {}
+
+    @java.lang.Override
+    public com.google.protobuf.Descriptors.FileDescriptor getFileDescriptor() {
+      return com.hy.user.pb.User.getDescriptor();
+    }
+
+    @java.lang.Override
+    public com.google.protobuf.Descriptors.ServiceDescriptor getServiceDescriptor() {
+      return getFileDescriptor().findServiceByName("UserService");
+    }
+  }
+
+  private static final class UserServiceFileDescriptorSupplier
+      extends UserServiceBaseDescriptorSupplier {
+    UserServiceFileDescriptorSupplier() {}
+  }
+
+  private static final class UserServiceMethodDescriptorSupplier
+      extends UserServiceBaseDescriptorSupplier
+      implements io.grpc.protobuf.ProtoMethodDescriptorSupplier {
+    private final String methodName;
+
+    UserServiceMethodDescriptorSupplier(String methodName) {
+      this.methodName = methodName;
+    }
+
+    @java.lang.Override
+    public com.google.protobuf.Descriptors.MethodDescriptor getMethodDescriptor() {
+      return getServiceDescriptor().findMethodByName(methodName);
+    }
+  }
+
+  private static volatile io.grpc.ServiceDescriptor serviceDescriptor;
+
+  public static io.grpc.ServiceDescriptor getServiceDescriptor() {
+    io.grpc.ServiceDescriptor result = serviceDescriptor;
+    if (result == null) {
+      synchronized (UserServiceGrpc.class) {
+        result = serviceDescriptor;
+        if (result == null) {
+          serviceDescriptor = result = io.grpc.ServiceDescriptor.newBuilder(SERVICE_NAME)
+              .setSchemaDescriptor(new UserServiceFileDescriptorSupplier())
+              .addMethod(METHOD_GET_USER_BY_NAME)
+              .build();
+        }
+      }
+    }
+    return result;
+  }
+}
