f8ac244d621a459fb06c267ebb4c05e80795f498
yuanhang3260 <yuanhang3260@gmail.com>
2019-10-24 02:27:52 +0800

    update

---
+++ user-service/src/main/java/com/hy/user/service/UserServiceImpl.java
@@ -0,0 +1,61 @@
+package com.hy.user.service;
+
+import com.hy.user.pb.UserInfo;
+import com.hy.user.pb.UserInfoRequest;
+import com.hy.user.pb.UserInfoResponse;
+import com.hy.user.pb.UserServiceGrpc;
+import io.grpc.stub.StreamObserver;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class UserServiceImpl extends UserServiceGrpc.UserServiceImplBase {
+
+  private Map<String, UserInfo> users = new HashMap<>();
+
+  public UserServiceImpl() {
+    UserInfo user = UserInfo.newBuilder()
+        .setName("hy")
+        .setAge(18)
+        .setGender("male")
+        .build();
+    users.put(user.getName(), user);
+
+    user = UserInfo.newBuilder()
+        .setName("panda")
+        .setAge(7)
+        .setGender("male")
+        .build();
+    users.put(user.getName(), user);
+
+    user = UserInfo.newBuilder()
+        .setName("snoopy")
+        .setAge(3)
+        .setGender("male")
+        .build();
+    users.put(user.getName(), user);
+  }
+
+  @Override
+  public void getUserByName(UserInfoRequest request, StreamObserver<UserInfoResponse> responseObserver) {
+    UserInfo user = users.get(request.getName());
+    UserInfoResponse response;
+    if (user != null) {
+      response = UserInfoResponse.newBuilder()
+          .setUser(user)
+          .setSuccess(true)
+          .build();
+    } else {
+      response = UserInfoResponse.newBuilder()
+          .setSuccess(false)
+          .setErrMsg("failed to get user " + request.getName())
+          .build();
+    }
+
+    responseObserver.onNext(response);
+    responseObserver.onCompleted();
+  }
+
+}
